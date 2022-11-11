32b6988557fd0cea6ced91660da02975f57fda31
AlexTrinityBlock <q123717111@gmail.com>
2022-11-11 17:54:57 +0800

    取消前端Cookie設置

--- codes/bank-django-service/app_core/static/login/script.js
+++ codes/bank-django-service/app_core/static/login/script.js
@@ -3,34 +3,6 @@ import CheckLoginStatus from '/static/login/CheckLoginStatus.js'
 
 // 送出函數
 function sendMessage(account_input, password_input) {
-    // // Fetch Get 函數
-    // // 不過由於 Get 參數要加在URL裏頭，所以要加問候並且使用URLSearchParams()來轉換。
-    // fetch("/api/login?" + new URLSearchParams({
-    //     account: account_input,
-    //     password: password_input,
-    // }),
-    //     {
-    //         // Get方法
-    //         method: "get",
-    //         // Header 一定要加入，否則在Laravel一類的框架可能會接收不到
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //             "Accept": "application/json",
-    //             "X-Requested-With": "XMLHttpRequest",
-    //         }
-    //     }).then((response) => {
-    //         // 將收到的回應轉換成JSON物件
-    //         return response.json();
-    //     }).then((jsonObj) => {
-    //         // 若登入成功
-    //         if (jsonObj['code'] == 1) {
-    //             Cookies.set('token', jsonObj['token'])
-    //             LoginFail.innerHTML = " "
-    //             window.location.replace("/");
-    //         } else {
-    //             LoginFail.innerHTML = "登入失敗，請重新登入"
-    //         }
-    //     });
     // Fetch函數
     const data = JSON.stringify({
         'account':account_input,
@@ -55,7 +27,7 @@ function sendMessage(account_input, password_input) {
     }).then((jsonObj) => {
         // 若登入成功
         if (jsonObj['code'] == 1) {
-            Cookies.set('token', jsonObj['token'])
+            // Cookies.set('token', jsonObj['token'])
             LoginFail.innerHTML = " "
             window.location.replace("/");
         } else {
