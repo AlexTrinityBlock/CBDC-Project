d04dc2eca086387d3d2e3b4002a625413d5b914d
xinyuyang1999 <chalcyyang1999@gmail.com>
2022-11-21 04:32:25 +0800

    增加使用者資料庫項目

--- codes/bank-django-service/app_core/models/User.py
+++ codes/bank-django-service/app_core/models/User.py
@@ -8,5 +8,12 @@ class User(models.Model):
     """
     account = models.CharField(max_length=100)
     password_hash = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    id_num = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
     e_mail = models.CharField(null=True,max_length= 100)
-    name = models.CharField(null=True,max_length= 100)
+    phone = models.CharField(max_length=100)
+    remaining = models.CharField(max_length=100)
+
+    
