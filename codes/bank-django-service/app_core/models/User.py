5a850906703218fece0de71dd38b15cdc8c3d4c1
xinyuyang1999 <110881230+xinyuyang1999@users.noreply.github.com>
2022-11-21 09:16:31 +0800

    資料表更新 (#11)
    * Yi 算法自檢程序修正
    
    * 增加使用者資料庫項目
    
    * 增加user資料項目
    
    * 新建商家、銀行簽章貨幣、使用者貨幣
    
    * 資料表更新
    
    * Update User.py
    
    Co-authored-by: AlexTrinityBlock <q123717111@gmail.com>
    Co-authored-by: AlexTrinityBlock <46709835+AlexTrinityBlock@users.noreply.github.com>

--- codes/bank-django-service/app_core/models/User.py
+++ codes/bank-django-service/app_core/models/User.py
@@ -8,5 +8,9 @@ class User(models.Model):
     """
     account = models.CharField(max_length=100)
     password_hash = models.CharField(max_length=100)
-    e_mail = models.CharField(null=True,max_length= 100)
     name = models.CharField(null=True,max_length= 100)
+    id_num = models.CharField(default="Identity Number",max_length=100)
+    home_address = models.CharField(default="Address",max_length=100)
+    e_mail = models.CharField(null=True,max_length= 100)
+    phone =models.CharField(default="09xxxxxxxx",max_length=100)
+    remaining = models.IntegerField(default=0)
