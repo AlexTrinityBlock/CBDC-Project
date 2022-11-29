17fa1bae885d88c017ff05ccfc090e8ae246f49e
AlexTrinityBlock <q123717111@gmail.com>
2022-11-29 15:29:41 +0800

    攝影機開發

--- codes/bank-django-service/app_core/static/pay/script.js
+++ codes/bank-django-service/app_core/static/pay/script.js
@@ -7,9 +7,22 @@ async function load_balance() {
     }, 1000);
 }
 
+// 啟動網路攝影機
+async function webCam() {
+    const html5QrCode = new Html5Qrcode("reader");
+
+    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+
+    html5QrCode.start({ facingMode: "environment" }, config, (decodedText, decodedResult) => {
+        console.log('Success')
+        html5QrCode.stop()
+    });
+}
+
 // 主函數
 function main() {
     load_balance();
+    webCam();
 }
 
 // 當頁面完全載入後啟動主函數
