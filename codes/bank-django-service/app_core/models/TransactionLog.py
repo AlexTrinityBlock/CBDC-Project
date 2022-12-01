b768cbbf720a27746a6ecdc3a8255c4fa4b0b883
AlexTrinityBlock <q123717111@gmail.com>
2022-12-01 17:50:35 +0800

    額外訊息資料表

--- codes/bank-django-service/app_core/models/TransactionLog.py
+++ codes/bank-django-service/app_core/models/TransactionLog.py
@@ -8,3 +8,4 @@ class TransactionLog(models.Model):
    status = models.IntegerField(null=True) # 狀態1或者0，1成功，0失敗。
    type = models.CharField(null=True,max_length=100) # 交易的型態，如提款存款
    used_currency = models.CharField(null=True,max_length=1000) # 若是存款，保留貨幣的內容。
+   message = models.CharField(null=True,max_length=1000) # 額外訊息
