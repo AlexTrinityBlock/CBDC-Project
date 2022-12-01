5e1d77f37b52fd616cc7565d919ff543f8224038
AlexTrinityBlock <q123717111@gmail.com>
2022-12-01 18:18:06 +0800

    Revert "修改自動化測試"
    This reverts commit 3bfb57dd1ba42579a909c34de648ba627ef1936a.

--- codes/bank-django-service/app_core/tests/test_login.py
+++ codes/bank-django-service/app_core/tests/test_login.py
@@ -28,46 +28,46 @@ class TestLogin(TestCase):
         self.redis_connection_token_index = redis.Redis(host=os.environ['REDIS_IP'], port=6379, db=0, password=os.environ['REDIS_PASSWORD'])
         self.redis_connection_user_index = redis.Redis(host=os.environ['REDIS_IP'], port=6379, db=1, password=os.environ['REDIS_PASSWORD'])
         
-    # # 測試登入
-    # def test_Login(self):
-    #     login = Login()
-    #     # 正確登入
-    #     print("[登入測試] 正確登入測試")
-    #     result = requests.post(self.login_url, data={'account': self.account, 'password':self.password})
-    #     result_json_object = json.loads(result.text)
-    #     self.assertEqual(result_json_object['code'], 1, '\n\n正確登入測試失敗!')
+    # 測試登入
+    def test_Login(self):
+        login = Login()
+        # 正確登入
+        print("[登入測試] 正確登入測試")
+        result = requests.post(self.login_url, data={'account': self.account, 'password':self.password})
+        result_json_object = json.loads(result.text)
+        self.assertEqual(result_json_object['code'], 1, '\n\n正確登入測試失敗!')
 
-    #     # user = User()
-    #     # user.account = 'user1'
-    #     # user.password_hash = '04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb'
-    #     # user.e_mail = '1235456464'
-    #     # user.save()
-    #     # rf = RequestFactory()
-    #     # post_request = rf.post('/api/login/', {'account': 'user1', 'password':'user'})
-    #     # # login = Login()
-    #     # response = login.login(post_request)
-    #     # print(response)
-    #     # result = json.loads(response.content.decode("utf-8"))
-    #     # self.assertEqual(result['code'], 1, '\n\n正確登入測試失敗!')
+        # user = User()
+        # user.account = 'user1'
+        # user.password_hash = '04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb'
+        # user.e_mail = '1235456464'
+        # user.save()
+        # rf = RequestFactory()
+        # post_request = rf.post('/api/login/', {'account': 'user1', 'password':'user'})
+        # # login = Login()
+        # response = login.login(post_request)
+        # print(response)
+        # result = json.loads(response.content.decode("utf-8"))
+        # self.assertEqual(result['code'], 1, '\n\n正確登入測試失敗!')
 
-    #     print("[登入測試] 檢查Redis是否正確存有Token")
-    #     token = self.redis_connection_user_index.get(self.account).decode('utf-8')
-    #     asserted_token = result_json_object['token']
-    #     self.assertEqual(token, asserted_token)
+        print("[登入測試] 檢查Redis是否正確存有Token")
+        token = self.redis_connection_user_index.get(self.account).decode('utf-8')
+        asserted_token = result_json_object['token']
+        self.assertEqual(token, asserted_token)
 
-    #     print("[登入測試] 測試登入檢查器")
-    #     result_check_login = requests.get(self.check_login_url, params={'token': token})
-    #     result_json_object_2 = json.loads(result_check_login.text)
-    #     self.assertEqual(result_json_object_2['code'], 1)
+        print("[登入測試] 測試登入檢查器")
+        result_check_login = requests.get(self.check_login_url, params={'token': token})
+        result_json_object_2 = json.loads(result_check_login.text)
+        self.assertEqual(result_json_object_2['code'], 1)
         
-    #     print("[登入測試] 刪除Token")
-    #     self.redis_connection_token_index.delete(token)
-    #     self.redis_connection_user_index.delete(self.account)
+        print("[登入測試] 刪除Token")
+        self.redis_connection_token_index.delete(token)
+        self.redis_connection_user_index.delete(self.account)
         
-    #     # 錯誤登入
-    #     print("[登入測試] 登入失敗判別測試")
-    #     result = requests.get(self.login_url, params={'account': self.account, 'password':'wrong password'})
-    #     result_json_object = json.loads(result.text)
-    #     self.assertEqual(result_json_object['code'], 0)
-    #     # self.assertTrue(False)
-    #     # self.assertFalse(False)
+        # 錯誤登入
+        print("[登入測試] 登入失敗判別測試")
+        result = requests.get(self.login_url, params={'account': self.account, 'password':'wrong password'})
+        result_json_object = json.loads(result.text)
+        self.assertEqual(result_json_object['code'], 0)
+        # self.assertTrue(False)
+        # self.assertFalse(False)
