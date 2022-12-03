d2f47883ac8d7fb4c48dcb40a36a86b8ff4b5412
AlexTrinityBlock <q123717111@gmail.com>
2022-12-03 15:00:17 +0800

    接受支付頁面，收款彈跳視窗。

--- codes/bank-django-service/app_core/static/pay_qr_code_page/script.js
+++ codes/bank-django-service/app_core/static/pay_qr_code_page/script.js
@@ -1,10 +1,28 @@
 import GetBalance from "/static/pay_qr_code_page/GetBalance.js"
 import GetPaymentID from "/static/pay_qr_code_page/GetPaymentID.js"
 
+var oldBalance = 0;
+
 // 載入餘額
 async function load_balance() {
+    // 取得貨幣
+    let balance = await GetBalance();
+    oldBalance = balance;
+    balance_text.innerHTML = '$' + balance
+
+    // 刷新貨幣額度
     window.setInterval(async () => {
-        balance_text.innerHTML = '$' + await GetBalance();
+        let balance = await GetBalance();
+
+        balance_text.innerHTML = '$' + balance
+        if (balance > oldBalance) {
+            Swal.fire({
+                icon: 'success',
+                title: '+ $'+(balance - oldBalance),
+            })
+        }
+
+        oldBalance = balance;
     }, 1000);
 }
 
@@ -12,15 +30,15 @@ async function load_balance() {
 async function qr_code() {
     let payment_id = await GetPaymentID()
     let qrcode = new QRCode(document.getElementById("qr_code"), {
-        colorDark : "#0066cc",
-    });	
-	qrcode.makeCode(payment_id);
+        colorDark: "#0066cc",
+    });
+    qrcode.makeCode(payment_id);
 }
 
 //顯示付款代碼
-async function display_payment_id(){
+async function display_payment_id() {
     let payment_id = await GetPaymentID()
-    payment_id_div.innerHTML = payment_id 
+    payment_id_div.innerHTML = payment_id
 }
 
 // 主函數
