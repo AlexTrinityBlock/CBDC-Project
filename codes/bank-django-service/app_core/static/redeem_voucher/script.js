e0120fec09b08338b89c88ae69a81a3fe16c75b1
AlexTrinityBlock <q123717111@gmail.com>
2022-12-03 14:46:58 +0800

    兌換點數

--- codes/bank-django-service/app_core/static/redeem_voucher/script.js
+++ codes/bank-django-service/app_core/static/redeem_voucher/script.js
@@ -1,33 +1,52 @@
-import GetBalance from "/static/pay_qr_code_page/GetBalance.js"
-import GetPaymentID from "/static/pay_qr_code_page/GetPaymentID.js"
+import GetBalance from "/static/redeem_voucher/GetBalance.js"
+import RedeemVoucher from "/static/redeem_voucher/RedeemVoucher.js"
 
 // 載入餘額
 async function load_balance() {
+    balance_text.innerHTML = '$' + await GetBalance();
     window.setInterval(async () => {
         balance_text.innerHTML = '$' + await GetBalance();
     }, 1000);
 }
 
-//建立 QR code
-async function qr_code() {
-    let payment_id = await GetPaymentID()
-    let qrcode = new QRCode(document.getElementById("qr_code"), {
-        colorDark : "#0066cc",
-    });	
-	qrcode.makeCode(payment_id);
+// QR code scanner 
+async function qr_scanner() {
+    const html5QrCode = new Html5Qrcode("reader");
+    const config = { fps: 10, qrbox: { width: 1000, height: 1000 } };
+    html5QrCode.start({ facingMode: "environment" }, config, (decodedText, decodedResult) => {
+        html5QrCode.stop()
+        process_2(decodedText)
+    });
 }
 
-//顯示付款代碼
-async function display_payment_id(){
-    let payment_id = await GetPaymentID()
-    payment_id_div.innerHTML = payment_id 
+// 支付步驟1
+async function process_1() {
+    qr_scanner();
+}
+
+// 支付步驟2
+async function process_2(decodedText) {
+    let redeemResult = await RedeemVoucher(decodedText)
+    console.log(redeemResult.code)
+    if (redeemResult.code == 1) {
+        Swal.fire({
+            icon: 'success',
+            title: 'Success !',
+        })
+    }
+    init()
+}
+
+// 初始化支付
+async function init() {
+    await new Promise(r => setTimeout(r, 500));
+    process_1();
 }
 
 // 主函數
 function main() {
     load_balance();
-    qr_code();
-    display_payment_id();
+    process_1();
 }
 
 // 當頁面完全載入後啟動主函數
