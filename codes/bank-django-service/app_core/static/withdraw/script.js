04e0afade6d250b9ef60132c6120c7b49d017c76
AlexTrinityBlock <q123717111@gmail.com>
2022-12-03 22:53:47 +0800

    產生 QR code 並強制確認

--- codes/bank-django-service/app_core/static/withdraw/script.js
+++ codes/bank-django-service/app_core/static/withdraw/script.js
@@ -20,23 +20,44 @@ async function uuidv4() {
     );
 }
 
+//建立 QR code
+async function qr_code(text) {
+    let qrcode = new QRCode(document.getElementById("qr_code"), {
+        colorDark: "#0066cc",
+        correctLevel : QRCode.CorrectLevel.L,
+    });
+    qrcode.makeCode(text);
+}
+
 // 提領貨幣
 async function handle_withdraw() {
     withdraw_btn.addEventListener('click', async function (e) {
         // 取得貨幣
         let withdraw_number = withdraw_input.value
         let token = await GetToken();//取得使用者識別
-        let currency = await Withdraw(token, withdraw_number,url);//取得貨幣
+        let currency = await Withdraw(token, withdraw_number, url);//取得貨幣
         balance_text.innerHTML = '$' + await GetBalance();//更新貨幣數量
         // 生成檔案
         var blob = new Blob([currency],
             { type: "text/plain;charset=utf-8" });
-        saveAs(blob, "貨幣."+await uuidv4()+".txt");
+        saveAs(blob, "貨幣." + await uuidv4() + ".txt");
         //  彈跳視窗
+
         Swal.fire({
             icon: 'success',
             title: '提款成功 !',
+            confirmButtonText: '取得QR code',
+            allowOutsideClick: false,
+        }).then((result) => {
+            Swal.fire({
+                title: '貨幣 QR code',
+                html: '<div id="qr_code"></div>',
+                confirmButtonText: '關閉',
+                allowOutsideClick: false,
+            })
+            qr_code(currency)
         })
+
         withdraw_input.value = 0
     }, false);
 }
