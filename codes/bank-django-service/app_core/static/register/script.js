b5dbeb52f820489dc757b45eac670b2d2d2469c0
AlexTrinityBlock <q123717111@gmail.com>
2022-12-06 12:26:42 +0800

    註冊檢查

--- codes/bank-django-service/app_core/static/register/script.js
+++ codes/bank-django-service/app_core/static/register/script.js
@@ -26,7 +26,7 @@ function sendMessage(account_input, password_input, e_mail_input) {
         // 若登入成功
         if (jsonObj['code'] == 1) {
             LoginFail.innerHTML = "註冊成功"
-            await new Promise(r => setTimeout(r, 2000));
+            await new Promise(r => setTimeout(r, 1000));
             window.location.replace("/login");
         } else if(jsonObj['code'] == 2){
             LoginFail.innerHTML = "帳號已經被註冊"
@@ -35,7 +35,7 @@ function sendMessage(account_input, password_input, e_mail_input) {
 }
 
 //處理密碼不吻合
-function handle_password_no_match(){
+function handlePasswordMatch(){
     if ( password.value != password_check.value){
         LoginFail.innerHTML = "兩次密碼不吻合"
         return false
@@ -43,9 +43,23 @@ function handle_password_no_match(){
     return true
 }
 
+// 檢查Email是否符合格式
+function handleValidateEmail() 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(e_mail.value))
+  {
+    return (true)
+  }
+  LoginFail.innerHTML = "E-mail 格式不符合"
+    return (false)
+}
+
+//
+
 // 將註冊送出
 function submit() {
-    if (!handle_password_no_match()){return} //如果兩次密碼不吻合則結束
+    if (!handleValidateEmail()){return} // Email格式不符合就結束
+    if (!handlePasswordMatch()){return} //如果兩次密碼不吻合則結束
     sendMessage(account.value, password.value, e_mail.value)
 }
 
